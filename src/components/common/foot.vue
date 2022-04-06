e1bb2dae2f8407460272776415bf3c29e5299032
zhigang <fdrgdf@foxmail.com>
2022-04-06 18:01:22 +0800

    docs

--- src/components/common/foot.vue
+++ src/components/common/foot.vue
@@ -1,19 +1,17 @@
 <template>
   <footer class="github-footer">
-    <a class="footer-link" href="https://github.com/GuoyiZhang/078-Vue-resumeVue">项目库托管在Github上，欢迎star~</a>
+    <a class="footer-link" href="https://github.com/cresentboy/online-resume"
+      >项目库托管在Github</a
+    >
   </footer>
 </template>
 <script>
-
-    export default {
-        name: 'foot',
-        data() {
-            return {
-            }
-        },
-        computed: {
-        },
-        mounted() {
-        },
-    }
+export default {
+  name: "foot",
+  data() {
+    return {};
+  },
+  computed: {},
+  mounted() {}
+};
 </script>
